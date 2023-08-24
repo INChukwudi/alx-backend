a810c93556b9401a1621b5b7a87e25f3e89e1a64
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-08-24 13:54:44 +0000

    added that task 2 solution file e, 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
 """
-Module containing the FIFOCache class
+Module containing the LIFOCache Class
 """
 
 from base_caching import BaseCaching
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
-    Class that defines an instance of the FIFOCache class
-    Inherits from the BaseCaching class
+    Defines the LIFOCache class instance
+    Inherits from the BaseCaching Class
     """
     def __init__(self):
         """
-        Initialize the FIFO Cache
-        """
+        Initialize the LIFOCache Class"""
         super().__init__()
-        self.keys_queue = []
+        self.keys_stack = []
 
     def put(self, key, item):
         """
-        Persists an item in the cache in FIFO order
+        Persists an item in the LIFO cache
 
         Args:
         key - identifier to store value under
@@ -29,20 +28,20 @@ class FIFOCache(BaseCaching):
         """
         if key is not None and item is not None:
             if key in self.cache_data:
-                self.keys_queue.remove(key)
+                self.keys_stack.remove(key)
                 self.cache_data[key] = item
-                self.keys_queue.append(key)
+                self.keys_stack.append(key)
                 return
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                oldest_key = self.keys_queue.pop(0)
-                self.cache_data.pop(oldest_key)
-                print("DISCARD: {}".format(oldest_key))
+                recent_key = self.keys_stack.pop()
+                self.cache_data.pop(recent_key)
+                print("DISCARD: {}".format(recent_key))
             self.cache_data[key] = item
-            self.keys_queue.append(key)
+            self.keys_stack.append(key)
 
     def get(self, key):
         """
-        Get an item by key
+        Get an item from the LIFO cache by key
 
         Args:
         key - identifier to look for in the cache
