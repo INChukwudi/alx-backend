a6c35de190f02570af246c4a57c35dba78e7631b
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-08-24 14:11:31 +0000

    added task 3 solution file, 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
-
 """
-Module containing the LIFOCache Class
+Module containing the LRUCache class
 """
 
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """
-    Defines the LIFOCache class instance
-    Inherits from the BaseCaching Class
+    Defines an instance of the LRUCache class
+    inherits from the BaseCaching class
     """
     def __init__(self):
         """
-        Initialize the LIFOCache Class"""
+        Initialize LRU Cache class instance
+        """
         super().__init__()
-        self.keys_stack = []
+        self.keys_lru_order = []
 
     def put(self, key, item):
         """
-        Persists an item in the LIFO cache
+        Persists an item in the LRU cache
 
         Args:
         key - identifier to store value under
@@ -28,20 +28,20 @@ class LIFOCache(BaseCaching):
         """
         if key is not None and item is not None:
             if key in self.cache_data:
-                self.keys_stack.remove(key)
+                self.keys_lru_order.remove(key)
                 self.cache_data[key] = item
-                self.keys_stack.append(key)
+                self.keys_lru_order.append(key)
                 return
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                recent_key = self.keys_stack.pop()
-                self.cache_data.pop(recent_key)
-                print("DISCARD: {}".format(recent_key))
+                lru_key = self.keys_lru_order.pop(0)
+                self.cache_data.pop(lru_key)
+                print("DISCARD: {}".format(lru_key))
             self.cache_data[key] = item
-            self.keys_stack.append(key)
+            self.keys_lru_order.append(key)
 
     def get(self, key):
         """
-        Get an item from the LIFO cache by key
+        Get an item by key
 
         Args:
         key - identifier to look for in the cache
@@ -50,5 +50,7 @@ class LIFOCache(BaseCaching):
         value - value stored under the key or None
         """
         if key is not None and key in self.cache_data:
+            self.keys_lru_order.remove(key)
+            self.keys_lru_order.append(key)
             return self.cache_data[key]
         return None
