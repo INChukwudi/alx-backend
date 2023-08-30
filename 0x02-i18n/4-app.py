632e6792a54da53b52430fd5370116ad23622cc7
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-08-30 20:22:06 +0000

    added task 4 related files

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+Module that contains definitions for application routes
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.config.from_object('config.Config')
+
+
+@app.route('/')
+def hello_world():
+    """
+    Returns the basic index page for the application
+    :return: 4-index.html template
+    """
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Checks for the current locale
+    :return: str
+    """
+    language = request.args.get('locale')
+
+    if language not in app.config["LANGUAGES"]:
+        return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+    return language
+
+
+if __name__ == '__main__':
+    app.run()
