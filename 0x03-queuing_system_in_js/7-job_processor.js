27fe82954a2ce92b6c26c74eec38a22abd5804f7
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-09-27 13:36:01 +0000

    added 7-job_processor.js to process the job created by 7-job_creator.js and tracks the progrss to 50%

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,30 @@
+import kue from 'kue';
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    const errorMessage = `Phone number ${phoneNumber} is blacklisted`;
+    done(new Error(errorMessage));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    setTimeout(() => {
+      done();
+    }, 1000);
+  }
+}
+
+const queue = kue.createQueue({ concurrency: 2 });
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
+
+queue.on('error', (err) => {
+  console.error('Job processor error:', err);
+});
+
